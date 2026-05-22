89818129f33a65eb18291c5f4e680954262596d2
test <139727181+ljs1990-tj@users.noreply.github.com>
2026-05-22 11:19:25 +0900

    2026.05.22

--- src/component/student/List.js
+++ src/component/student/List.js
@@ -1,17 +1,41 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import '../../App.css';
 
 function StudentList(){
-
+    let [list, setList] = useState([]);
     function stuList(){
         fetch("http://localhost:3010/student")
             .then(res => res.json())
             .then(data => {
                 console.log(data);
+                setList(data.list);
             });
     }
-    stuList();
+    useEffect(()=>{
+        stuList();
+    }, []);
     return <>
         <h2>학생 목록!</h2>
+        <div>
+            <table>
+                <tr>
+                    <th>학번</th>
+                    <th>이름</th>
+                    <th>학과</th>
+                    <th>학년</th>
+                </tr>
+                {list.map(item => {
+                    return <tr>
+                        <td>{item.STU_NO}</td>
+                        <td>{item.STU_NAME}</td>
+                        <td>{item.STU_DEPT}</td>
+                        <td>{item.STU_GRADE}</td>
+                    </tr>
+                })}
+            </table>
+        </div>
+
         <hr></hr>
         <Link to="/student/add">학생추가</Link>
     </>
