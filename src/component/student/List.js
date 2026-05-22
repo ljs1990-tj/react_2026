6b24dc6e092ba286f5df897a3a69d149ecdcad9a
test <139727181+ljs1990-tj@users.noreply.github.com>
2026-05-22 13:16:27 +0900

    2026.05.22

--- src/component/student/List.js
+++ src/component/student/List.js
@@ -1,11 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import '../../App.css';
 
 function StudentList(){
     let [list, setList] = useState([]);
+    let [keyword, setKeyword] = useState("");
+    let keywordRef = useRef("");
     function stuList(){
-        fetch("http://localhost:3010/student")
+        fetch("http://localhost:3010/student?keyword="+keyword)
             .then(res => res.json())
             .then(data => {
                 console.log(data);
@@ -14,10 +16,18 @@ function StudentList(){
     }
     useEffect(()=>{
         stuList();
-    }, []);
+    }, [keyword]);
     return <>
         <h2>학생 목록!</h2>
         <div>
+            {/* 이름 검색 */}
+            {/* <input ref={keywordRef}></input> */}
+            <input value={keyword} onChange={(e)=>{
+                setKeyword(e.target.value);
+            }}></input>
+            <button onClick={()=>{
+                stuList();
+            }}>검색</button>
             <table>
                 <tr>
                     <th>학번</th>
